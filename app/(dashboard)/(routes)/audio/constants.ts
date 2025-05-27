7b2b5719a4981316a630e47dbb53297bc34bdf34
rakshith <152013282+rakshithgowdams368@users.noreply.github.com>
2025-05-27 21:14:42 +0530

    Update constants.ts

--- app/(dashboard)/(routes)/audio/constants.ts
+++ app/(dashboard)/(routes)/audio/constants.ts
@@ -1,5 +1,5 @@
-//file path :-ai-saas-antonio-main/app/(dashboard)/(routes)/audio/constants.ts
-import * as z from "zod";
+// app/(dashboard)/(routes)/audio/constants.ts
+import { z } from "zod";
 
 export const formSchema = z.object({
   prompt: z.string().min(1, {
